9f03f76ccca80b7cd1aa2d7fbbfdf05154199f84
Águeda <aguedapl55@educastur.es>
2024-01-30 20:08:39 +0100

    Clase J25/01/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuInicialComponent } from './menu-inicial/menu-inicial.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuInicialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatToolbarModule, 
+    MatIconModule, 
+    MatTabsModule, 
+    MatProgressBar
   ],
   providers: [],
   bootstrap: [AppComponent]
